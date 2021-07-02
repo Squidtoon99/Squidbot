f8ed832369939a7c5ed772d282dcdf41574543e4
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2021-07-02 15:53:40 -0500

    adding source and ping commands

---
+++ cogs/utility.py
@@ -0,0 +1,17 @@
+from ink.core import squidcommand 
+from discord.ext import commands
+
+class Utility(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot 
+    
+    @squidcommand() 
+    async def source(self, ctx):
+        yield "https://github.com/Squidtoon99/SquidBot"
+    
+    @squidcommand() 
+    async def ping(self, ctx):
+        yield "Bot Latency: `{:,}` ms".format(int(self.bot.latency*1000))
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
